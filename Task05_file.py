a1e50d714ae676a10afdb207b48fa4789ef9bd8e
Kyriakos Markianos <kyriakos.markianos@proton.me>
2024-10-05 16:12:39 +0100

    Added new file Task05_file.py in the repo for visibility

---
+++ Task05_file.py
@@ -0,0 +1,12 @@
+print('''
+This is the file in My_Git, creted in VSCode.
+Working on getting this visible on the remote side
+(which uses the Git with the CLI)
+      ''')
+
+print('''
+**** SUCCESS ****
+
+I can see the file in the file system on the remote side. 
+      
+''')
